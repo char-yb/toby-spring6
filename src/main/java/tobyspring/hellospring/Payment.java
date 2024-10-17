a8196f9943a13aedd533f6478ee527111b2cfb45
ybchar <uiurihappy@naver.com>
2024-10-18 00:07:30 +0900

    feat: payment constructor, getter, toString 정의

--- src/main/java/tobyspring/hellospring/Payment.java
+++ src/main/java/tobyspring/hellospring/Payment.java
@@ -1,4 +1,67 @@
 package tobyspring.hellospring;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
 public class Payment {
+
+	// 주문 번호
+	private Long orderId;
+	// 외환 통화
+	private String currency;
+	// 결제 금액 (Double 타입 사용 금지)
+	// 왜? Double 타입은 부동소수점 방식을 사용하며, 이는 정확한 금액을 표현할 수 없기 때문입니다.
+	private BigDecimal amount;
+	// 환율
+	private BigDecimal exRate;
+	// 결제 금액 (원화)
+	private BigDecimal convertedAmount;
+	// 유효기간
+	private LocalDateTime validUntil;
+
+	public Long getOrderId() {
+		return orderId;
+	}
+
+	public String getCurrency() {
+		return currency;
+	}
+
+	public BigDecimal getAmount() {
+		return amount;
+	}
+
+	public BigDecimal getExRate() {
+		return exRate;
+	}
+
+	public BigDecimal getConvertedAmount() {
+		return convertedAmount;
+	}
+
+	public LocalDateTime getValidUntil() {
+		return validUntil;
+	}
+
+	public Payment(Long orderId, String currency, BigDecimal amount, BigDecimal exRate, BigDecimal convertedAmount,
+		LocalDateTime validUntil) {
+		this.orderId = orderId;
+		this.currency = currency;
+		this.amount = amount;
+		this.exRate = exRate;
+		this.convertedAmount = convertedAmount;
+		this.validUntil = validUntil;
+	}
+
+	@Override
+	public String toString() {
+		return "Payment{" +
+			"orderId=" + orderId +
+			", currency='" + currency + '\'' +
+			", amount=" + amount +
+			", exRate=" + exRate +
+			", convertedAmount=" + convertedAmount +
+			", validUntil=" + validUntil +
+			'}';
+	}
 }
--- src/main/java/tobyspring/hellospring/PaymentService.java
+++ src/main/java/tobyspring/hellospring/PaymentService.java
@@ -1,14 +1,35 @@
 package tobyspring.hellospring;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
 public class PaymentService {
 
-	public Payment prepare() {
-		return new Payment();
+	public Payment prepare(
+		Long orderId,
+		String currency,
+		BigDecimal foreignCurrencyAmount
+	) {
+		// 1. 환율 가져오기
+		// 2. 금액 계산
+		// 3. 유효 시간 계산
+		return new Payment(
+			orderId,
+			currency,
+			foreignCurrencyAmount,
+			BigDecimal.valueOf(1200),
+			foreignCurrencyAmount.multiply(BigDecimal.valueOf(1200)),
+			LocalDateTime.now()
+		);
 	}
 
 	public static void main(String[] args) {
 		PaymentService paymentService = new PaymentService();
-		Payment payment = paymentService.prepare();
+		Payment payment = paymentService.prepare(
+			100L,
+			"USD",
+			BigDecimal.valueOf(50.7)
+		);
 		System.out.println(payment);
 	}
 }
