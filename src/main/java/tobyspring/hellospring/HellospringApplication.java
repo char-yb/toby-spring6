8e44a84a2fd945defb3fa6fb2a4df3ac3eaead6a
ybchar <uiurihappy@naver.com>
2024-10-20 01:13:24 +0900

    chore: spotless 적용

--- src/main/java/tobyspring/hellospring/HellospringApplication.java
+++ src/main/java/tobyspring/hellospring/HellospringApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class HellospringApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HellospringApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(HellospringApplication.class, args);
+    }
 }
--- src/main/java/tobyspring/hellospring/PaymentService.java
+++ src/main/java/tobyspring/hellospring/PaymentService.java
@@ -5,31 +5,22 @@ import java.time.LocalDateTime;
 
 public class PaymentService {
 
-	public Payment prepare(
-		Long orderId,
-		String currency,
-		BigDecimal foreignCurrencyAmount
-	) {
-		// 1. 환율 가져오기
-		// 2. 금액 계산
-		// 3. 유효 시간 계산
-		return new Payment(
-			orderId,
-			currency,
-			foreignCurrencyAmount,
-			BigDecimal.valueOf(1200),
-			foreignCurrencyAmount.multiply(BigDecimal.valueOf(1200)),
-			LocalDateTime.now()
-		);
-	}
+    public Payment prepare(Long orderId, String currency, BigDecimal foreignCurrencyAmount) {
+        // 1. 환율 가져오기
+        // 2. 금액 계산
+        // 3. 유효 시간 계산
+        return new Payment(
+                orderId,
+                currency,
+                foreignCurrencyAmount,
+                BigDecimal.valueOf(1200),
+                foreignCurrencyAmount.multiply(BigDecimal.valueOf(1200)),
+                LocalDateTime.now());
+    }
 
-	public static void main(String[] args) {
-		PaymentService paymentService = new PaymentService();
-		Payment payment = paymentService.prepare(
-			100L,
-			"USD",
-			BigDecimal.valueOf(50.7)
-		);
-		System.out.println(payment);
-	}
+    public static void main(String[] args) {
+        PaymentService paymentService = new PaymentService();
+        Payment payment = paymentService.prepare(100L, "USD", BigDecimal.valueOf(50.7));
+        System.out.println(payment);
+    }
 }
