74ea682387317d7a4793e0c9c4330fcd7b2b3d14
ybchar <uiurihappy@naver.com>
2024-10-23 21:42:00 +0900

    feat: PaymentService

---
+++ src/main/java/tobyspring/hellospring/ExRateData.java
@@ -0,0 +1,9 @@
+package tobyspring.hellospring;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import java.math.BigDecimal;
+import java.util.Map;
+
+// json 데이터에서 다른 필드가 추가되더라도 무시하고 필요한 생성자에 선언된 필드만 사용한다.
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record ExRateData(String result, Map<String, BigDecimal> rates) {}
--- src/main/java/tobyspring/hellospring/PaymentService.java
+++ src/main/java/tobyspring/hellospring/PaymentService.java
@@ -1,24 +1,44 @@
 package tobyspring.hellospring;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.math.BigDecimal;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.time.LocalDateTime;
+import java.util.stream.Collectors;
 
 public class PaymentService {
 
-    public Payment prepare(Long orderId, String currency, BigDecimal foreignCurrencyAmount) {
+    public Payment prepare(Long orderId, String currency, BigDecimal foreignCurrencyAmount)
+            throws IOException {
         // 1. 환율 가져오기
+        // 환율 사이트: https://open.er-api.com/v6/latest/USD
+        URL url = new URL("https://open.er-api.com/v6/latest/" + currency);
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection(); // casting
+        BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        String response = br.lines().collect(Collectors.joining());
+        br.close(); // stream은 열어주면 닫아줘야 한다.
+
+        ObjectMapper mapper = new ObjectMapper();
+        ExRateData data = mapper.readValue(response, ExRateData.class);
+
+        BigDecimal exRate = data.rates().get("KRW");
+        System.out.println(exRate);
+
         // 2. 금액 계산
+        BigDecimal convertedAmount = foreignCurrencyAmount.multiply(exRate);
+
         // 3. 유효 시간 계산
+        LocalDateTime validUntil = LocalDateTime.now().plusMinutes(30);
+
         return new Payment(
-                orderId,
-                currency,
-                foreignCurrencyAmount,
-                BigDecimal.valueOf(1200),
-                foreignCurrencyAmount.multiply(BigDecimal.valueOf(1200)),
-                LocalDateTime.now());
+                orderId, currency, foreignCurrencyAmount, exRate, convertedAmount, validUntil);
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         PaymentService paymentService = new PaymentService();
         Payment payment = paymentService.prepare(100L, "USD", BigDecimal.valueOf(50.7));
         System.out.println(payment);
