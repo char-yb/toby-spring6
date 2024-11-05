a0383e5663130e3f02c1a09b64cf43c57a5e0c7b
ybchar <uiurihappy@naver.com>
2024-11-05 16:01:14 +0900

    test: AutoWired를 사용해 Stub 객체 테스트

--- src/test/java/tobyspring/hellospring/payment/PaymentServiceSpringTest.java
+++ src/test/java/tobyspring/hellospring/payment/PaymentServiceSpringTest.java
@@ -6,7 +6,6 @@ import java.io.IOException;
 import java.math.BigDecimal;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
@@ -17,12 +16,13 @@ import tobyspring.hellospring.TestObjectFactory;
 @ContextConfiguration(classes = TestObjectFactory.class)
 class PaymentServiceSpringTest {
 
-    @Autowired BeanFactory beanFactory;
+    @Autowired PaymentService paymentService;
+    // 테스트를 위한 용도의 클래스를 선언하였고, 그에 따른 setter로 조작이 가능하다.
+    @Autowired ExRateProviderStub exRateProviderStub;
 
     @Test
     void convertedAmount() throws IOException {
-        PaymentService paymentService = beanFactory.getBean(PaymentService.class);
-
+        // Stub에 기본적으로 세팅한 값으로 사용
         Payment payment = paymentService.prepare(1L, "USD", BigDecimal.TEN);
 
         // BigDecimal에서는 isEqualTo를 사용해선 안된다. 내부 부동 소수점이나 반올림 문제로 인해 오류가 발생할 수 있다.
@@ -32,5 +32,18 @@ class PaymentServiceSpringTest {
         assertThat(payment.getExRate()).isEqualByComparingTo(BigDecimal.valueOf(1_000));
         // 원화환산 금액 계산
         assertThat(payment.getConvertedAmount()).isEqualByComparingTo(BigDecimal.valueOf(10_000));
+
+        // exRate: 500
+        // Stub에 기본적으로 세팅한 값으로 사용
+        exRateProviderStub.setExRate(BigDecimal.valueOf(500));
+        Payment payment2 = paymentService.prepare(1L, "USD", BigDecimal.TEN);
+
+        // BigDecimal에서는 isEqualTo를 사용해선 안된다. 내부 부동 소수점이나 반올림 문제로 인해 오류가 발생할 수 있다.
+        // 그래서 사용되는 것이 isEqualByComparingTo이다.
+
+        // 환율 정보 가져온다.
+        assertThat(payment2.getExRate()).isEqualByComparingTo(BigDecimal.valueOf(500));
+        // 원화환산 금액 계산
+        assertThat(payment2.getConvertedAmount()).isEqualByComparingTo(BigDecimal.valueOf(5_000));
     }
 }
