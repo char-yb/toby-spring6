7e4683302f4afb6b44857bc08592b1a6dc6edc3a
ybchar <uiurihappy@naver.com>
2024-11-17 01:08:10 +0900

    refactor: Exception try-catch

--- src/main/java/tobyspring/hellospring/exRate/WebApiExRateProvider.java
+++ src/main/java/tobyspring/hellospring/exRate/WebApiExRateProvider.java
@@ -1,11 +1,14 @@
 package tobyspring.hellospring.exRate;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.math.BigDecimal;
 import java.net.HttpURLConnection;
 import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.stream.Collectors;
 import tobyspring.hellospring.payment.ExRateProvider;
 
@@ -15,26 +18,33 @@ public class WebApiExRateProvider implements ExRateProvider {
     // 의존관계
     @Override
     public BigDecimal getExRate(String currency) {
+        String url = "https://open.er-api.com/v6/latest/" + currency;
+        URI uri;
         try {
-            URI uri =
-                    new URI(
-                            "https://open.er-api.com/v6/latest/"
-                                    + currency); // 21부터는 URL가 아닌 URI를 사용
+            uri = new URI(url); // 21부터는 URL가 아닌 URI를 사용
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
 
+        String response;
+        try {
             HttpURLConnection connection =
                     (HttpURLConnection) uri.toURL().openConnection(); // casting
-            BufferedReader br =
-                    new BufferedReader(new InputStreamReader(connection.getInputStream()));
-            String response = br.lines().collect(Collectors.joining());
-            br.close(); // stream은 열어주면 닫아줘야 한다.
+            try (BufferedReader br =
+                    new BufferedReader(new InputStreamReader(connection.getInputStream()))) {
+                response = br.lines().collect(Collectors.joining());
+            }
+            // br.close(); // stream은 열어주면 닫아줘야 한다.
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
 
+        try {
             ObjectMapper mapper = new ObjectMapper();
             ExRateData data = mapper.readValue(response, ExRateData.class);
 
-            System.out.println("API ExRate: " + data.rates().get("KRW"));
-
             return data.rates().get("KRW");
-        } catch (Exception e) {
+        } catch (JsonProcessingException e) {
             throw new RuntimeException(e);
         }
     }
