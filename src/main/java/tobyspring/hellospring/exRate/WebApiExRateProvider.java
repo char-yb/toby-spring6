f612cc3defcba14521bff7ceae9f9d5515dce0e8
ybchar <uiurihappy@naver.com>
2024-11-18 23:12:58 +0900

    refactor: Mutable Code Extract Method

--- src/main/java/tobyspring/hellospring/exRate/WebApiExRateProvider.java
+++ src/main/java/tobyspring/hellospring/exRate/WebApiExRateProvider.java
@@ -28,25 +28,32 @@ public class WebApiExRateProvider implements ExRateProvider {
 
         String response;
         try {
-            HttpURLConnection connection =
-                    (HttpURLConnection) uri.toURL().openConnection(); // casting
-            try (BufferedReader br =
-                    new BufferedReader(new InputStreamReader(connection.getInputStream()))) {
-                response = br.lines().collect(Collectors.joining());
-            }
-            // br.close(); // stream은 열어주면 닫아줘야 한다.
+            response = executeApi(uri);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
 
         try {
-            ObjectMapper mapper = new ObjectMapper();
-            ExRateData data = mapper.readValue(response, ExRateData.class);
-
-            return data.rates().get("KRW");
+            return getParseKRWExRate(response);
         } catch (JsonProcessingException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    private static BigDecimal getParseKRWExRate(String response) throws JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper();
+        ExRateData data = mapper.readValue(response, ExRateData.class);
+        return data.rates().get("KRW");
+    }
+
+    private static String executeApi(URI uri) throws IOException {
+        String response;
+        HttpURLConnection connection = (HttpURLConnection) uri.toURL().openConnection(); // casting
+        try (BufferedReader br =
+                new BufferedReader(new InputStreamReader(connection.getInputStream()))) {
+            response = br.lines().collect(Collectors.joining());
+        }
+        return response;
     }
 }
 
