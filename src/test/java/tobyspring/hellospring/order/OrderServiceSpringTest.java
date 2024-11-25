7396fd062e515bd3adb29e35378ac54d7b50e312
ybchar <uiurihappy@naver.com>
2024-11-25 17:09:03 +0900

    refactor: OrderRepository DIP

---
+++ src/test/java/tobyspring/hellospring/order/OrderServiceSpringTest.java
@@ -0,0 +1,23 @@
+package tobyspring.hellospring.order;
+
+import java.math.BigDecimal;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import tobyspring.hellospring.OrderConfig;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = OrderConfig.class)
+public class OrderServiceSpringTest {
+
+    @Autowired OrderService orderService;
+
+    @Test
+    void createOrder() {
+        var order = orderService.createOrder("1234", BigDecimal.ONE);
+        Assertions.assertThat(order.getId()).isGreaterThan(0);
+    }
+}
